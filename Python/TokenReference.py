c0d7a79b35d5fb5c29dd2e1560de45b86a3a7979
Brian McMichael <brian@brianmcmichael.com>
2018-11-27 16:28:48 -0500

    Provide documentation for creating and sending a raw transaction.

--- Python/TokenReference.py
+++ Python/TokenReference.py
@@ -3,9 +3,7 @@ import json
 
 # Users will need to install web3 and eth_account
 # `pip install web3`
-# `pip install eth_account`
 from web3 import Web3
-from eth_account import Account
 
 
 # Users will need to create a free account at Infura (https://infura.io/) 
@@ -21,10 +19,11 @@ web3 = Web3(Web3.HTTPProvider(PROVIDER_ENDPOINT))
 #  enough ether to make a request from the contract.
 PRIVATE_KEY = "<PRIVATE_KEY_HERE>"
 
-# Use the eth_account library to derive the public address from the key
-# Web3.py doesn't include an option to convert a private key to a wallet address
-#  in memory, so we're relying on the Account object from the eth_account library
-ACCOUNT = Account.privateKeyToAccount(PRIVATE_KEY)
+
+# Use web3.eth.account library to derive the public address from the key.
+# 
+# https://web3py.readthedocs.io/en/stable/web3.eth.account.html
+ACCOUNT = web3.eth.account.privateKeyToAccount(PRIVATE_KEY)
 print(f"Wallet address: {ACCOUNT.address}")
 
 
@@ -92,3 +91,102 @@ print(f"Contract totalSupply: {contract_totalSupply}")
 # Let's check our CBUS token balance
 token_balance = columbusTokenContract.functions.balanceOf(ACCOUNT.address).call()
 print(f"My CBUS balance: {token_balance}")
+
+###############################################################################
+print("Building a Raw Transaction")
+
+# Functions that only read data from the blockchain can be invoked with the .call() function.
+# Functions that will modify the blockchain state will need to be prepared as transactions.
+
+# Now we're going to make our first transaction. This will prepare a raw transaction that requests
+#  one ColumbusToken from the contract!
+
+# First we will want to set some transaction variables.
+
+# We'll use this variable to set the maximum amount of gas to be used in the transaction. This
+#  generally prevents your costs from unexpectedly getting out of control by killing the transaction
+#  when the gas limit is exceeded.
+# For the ColumbusToken, invoking the getToken() method should cost around 69476 gas, so we'll estimate
+#  just a little more than that.
+maximum_gas = 100000
+
+# A nonce value is required for each transaction. It prevents duplicate transactions from being processed
+#  by the network and ensures that transactions are processed in the intended order. The nonce has a
+#  0-index and should be equal to the number of transactions sent by the account.
+# We can use web3's getTransactionCount(address) function to get the nonce value for a transaction.
+nonce = web3.eth.getTransactionCount(ACCOUNT.address)
+print(f"nonce: {nonce}")
+
+# The chainId is the id of the network we are deploying to, examples:
+#   1 : Ethereum Mainnet
+#   3 : Ropsten Testnet
+#   4 : Rinkeby Testnet
+# The web3 call returns the value as a string, but we will need an integer to create a transaction.
+chain_id = int(web3.version.network)
+print(f"Chain ID: {chain_id}")
+
+
+# Gas price is defined programatically in wei, but it's typically discussed in GWEI.
+#  Luckily, web3 provides us a method to quickly convert gwei to wei.
+#  Users may want to check https://ethgasstation.info/ prior to sending a transaction
+#  to ensure that they don't set too high or low a price.
+#  A gas price that's too high can incur needless expense.
+#  A price that's too low will mean that your transaction takes longer to process, and
+#  if it's very low it may never get processed at all.
+gas_price = web3.toWei('10', 'gwei')
+print(f"Gas Price: {gas_price} wei")
+
+
+# We're now going to build the transaction to request a ColumbusToken.
+# We will first build a raw transaction containing the data to be transmitted, and then
+#  we will cryptographically sign the transaction with our private key.
+#  
+#  https://web3py.readthedocs.io/en/stable/contracts.html#web3.contract.ContractFunction.buildTransaction
+#
+# The ColumbusToken contract contains a unique (and nonstandard) feature that allows
+#  an account to claim one CBUS token once upon request. This can actually be abused pretty
+#  easily so it's just a fun way to demonstrate interacting with a smart contract.
+#
+# See Line 57 of the code at:
+# https://etherscan.io/address/0x07c344edd719a356775e1fbd852c63dc46167b76#code 
+#
+#      function getToken() public {
+#        // Don't allow more tokens to be created than the maximum supply
+#        require(_totalSupply < max_supply);
+#        // Don't allow the requesting address to request more than one token.
+#        require(_requested[msg.sender] == false);
+#        
+#        _requested[msg.sender] = true;
+#        _balances[msg.sender] += _tokens(1);
+#        _totalSupply += _tokens(1);
+#        
+#        emit GeneratedToken(msg.sender);
+#      }
+raw_transaction = columbusTokenContract.functions.getToken().buildTransaction({
+    'chainId': chain_id,
+    'gas': maximum_gas,
+    'gasPrice': gas_price,
+    'nonce': nonce
+})
+print(f"Raw Transaction: {raw_transaction}\n")
+# Raw Transaction: {'value': 0, 'chainId': 1, 'gas': 100000, 'gasPrice': 10000000000, 
+#  'nonce': 0, 'to': '0x07c344edD719A356775E1FBd852c63Dc46167B76', 'data': '0x21df0da7'}
+
+
+# Sign the raw transaction using your private key using your web3 acount object
+signed_transaction = ACCOUNT.signTransaction(raw_transaction)
+print(f"Signed Transaction: {signed_transaction}\n")
+# Signed Transaction: AttrDict({'rawTransaction': 
+#  HexBytes('0xf86980850........76ae1525a0'), 
+#  'hash': HexBytes('0xa1845a370.......0d6b93f7727035f883cfa'), 
+#  'r': 93095648781695009.......6984224547883143517748, 
+#  's': 4683787713977.....27619885118225990304441992291744,
+#  'v': 38})
+
+
+# Your signed transaction contains everything it needs to be processed by the network.
+# To get your CBUS Token, uncomment the following line to submit the transaction.
+#  (Ensure that there is some ether in your account!)
+# Once the transaction has processed, go back and check your CBUS token balance.
+#transaction_hash = web3.eth.sendRawTransaction(signed_transaction.rawTransaction)
+#print(f"Transaction Hash: {transaction_hash}\n")
